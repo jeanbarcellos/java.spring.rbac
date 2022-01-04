17769e5469c1d1cd433e90f1860efd0676e166dd
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-03 23:31:13 -0300

    domain - pré-definição de Roles

---
+++ src/main/java/com/jeanbarcellos/processmanager/config/Roles.java
@@ -0,0 +1,13 @@
+package com.jeanbarcellos.processmanager.config;
+
+public class Roles {
+
+    // Somente desenvolvedor ou dono do software
+    public static final String ROOT = "root";
+
+    public static final String ADMINISTRATOR = "admin";
+    public static final String AUTHOR = "author";
+    public static final String EDITOR = "editor";
+    public static final String VIEWER = "viewer";
+
+}
